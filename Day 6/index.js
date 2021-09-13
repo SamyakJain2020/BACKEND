5c8859683dab6e20e15f86f1c6ee0c3a771fcbfa
SamyakJain2020 <samyakjainking@gmail.com>
2021-09-13 20:03:45 +0530

    THA 5 and 6

---
+++ Day 6/index.js
@@ -0,0 +1,10 @@
+// console.log("hello");
+// const express =require("express");
+// const app=express();
+// app.get('/',(req,res)=>{
+//     res.send("hello");
+// })
+// app.get('/samyak',(req,res)=>{
+//     res.send("in samyak");
+// })
+// app.listen(5000)
